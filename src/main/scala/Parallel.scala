490fa46c23635806fb069a608284ac19f4db8432
Daniel <danya.ostap@gmail.com>
2022-11-20 21:47:32 +0200

    Parallel and sequential operations comparison

--- src/main/scala/Parallel.scala
+++ src/main/scala/Parallel.scala
@@ -1,11 +1,14 @@
 package ua.nure.parallel
 
+import scala.collection.parallel.ParSeq
+import scala.util.Random
+
 object Parallel {
   def multiThreadedIdiomatic(m1:Seq[Array[Double]], m2:Array[Array[Double]] ) ={
     val res =  Array.fill(m1.length, m2(0).length)(0.0)
-    for(row <- (0 until m1.length).par;
-        col <- (0 until m2(0).length).par;
-        i   <- 0 until m1(0).length){
+    for(row <- m1.indices.par;
+        col <- m2(0).indices.par;
+        i <- m1.head.indices){
       res(row)(col) += m1(row)(i) * m2(i)(col)
     }
     res
@@ -15,9 +18,24 @@ object Parallel {
 //    val m1 = Array.fill(5,5) { Random.nextDouble() }
 //    val m2 = Array.fill(5,5) { Random.nextDouble() }
 
-    val m1 = Array(Array(2.0, 1.0), Array(1.0, 4.0));
-    val m2 = Array(Array(1.0, 2.0, 0.0), Array(0.0, 1.0, 2.0));
+    val SIZE = 4096 * 4096
+    val grades: Seq[Double] = Array.fill(SIZE) { Random.nextDouble() }
+    val gradesPar: ParSeq[Double] = grades.par
 
-    multiThreadedIdiomatic(m1,m2) foreach { row => row foreach print; println }
+    var t1 = System.nanoTime
+    val gradesReducedPar = gradesPar.map(_ - 5)
+    var duration = (System.nanoTime - t1) / 1e9d
+    println("Parallel time: " + duration + "s")
+
+    t1 = System.nanoTime
+    val gradesReducedSeq = grades.map(_ - 5)
+    duration = (System.nanoTime - t1) / 1e9d
+    println("Sequential time: " + duration + "s")
+
+
+//    val (m1, m2) = (Array(Array(2.0, 1.0), Array(1.0, 4.0)), Array(Array(1.0, 2.0, 0.0), Array(0.0, 1.0, 2.0)));
+//    val m2 = Array(Array(1.0, 2.0, 0.0), Array(0.0, 1.0, 2.0));
+
+//    multiThreadedIdiomatic(m1,m2) foreach { row => row foreach print; println }
   }
 }
